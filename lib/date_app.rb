8a784f28c15503b626372fb7a6019f949de6d1b5
Ｔ・Ｋ <karuma.reason@gmail.com>
2026-04-24 13:09:52 +0900

    Rename.

---
+++ lib/date_app.rb
@@ -0,0 +1,16 @@
+require 'date'
+
+class WatchDate
+  attr_reader :app
+
+  def initialize
+    @app = Time.now
+  end
+
+  def remove
+    remove_instance_variable(:@app)
+  end
+end
+
+# _posts date timestamp
+# 2025-01-13 13:39:34 +0900
