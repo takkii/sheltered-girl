91f1324e2d8de78de7baf47de55a6938956d4816
Ｔ・Ｋ <karuma.reason@gmail.com>
2026-05-26 07:53:04 +0900

    Update.

--- lib/json_post.rb
+++ lib/json_post.rb
@@ -1,13 +1,15 @@
+# frozen_string_literal: true
+
 require 'net/http'
 require 'uri'
 require 'json'
 
-member = File.expand_path(ARGV[1])
+member = File.expand_path("#{ARGV[1]}")
 
 File.open(member) do |f|
    name = JSON.load(f)
    response = Net::HTTP.post(
-      URI(ARGV[2]),
+      URI("#{ARGV[2]}"),
       name.to_json,
       'Content-Type' => 'application/json'
    )
