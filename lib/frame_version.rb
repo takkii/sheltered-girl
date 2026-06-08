b3e5994fb61ef4c26cdf9a09359e21e5150aa6c5
Ｔ・Ｋ <karuma.reason@gmail.com>
2026-06-08 10:19:07 +0900

    Update.

--- lib/frame_version.rb
+++ lib/frame_version.rb
@@ -3,19 +3,17 @@
 require 'date'
 
 begin
-
 # nyasocom_frame version = x.x.x-xxxx.xx.xx
 module Frame
-  frame_version = '1.2.3'
+  frame_version = '1.3.0'
   t = Date.today
   build_day = t.strftime('%Y.%m.%d')
   FRAME_VERSION = "#{frame_version}-#{build_day}".freeze
 end
-
 rescue StandardError => e
   puts e.backtrace
 ensure
-  GC.compact
+  GC.auto_compact
 end
 
 __END__
